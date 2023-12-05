1e8d86681351511ac764fc1769d8acaaaa524b08
Igor Oganesian <igor.oganesian@gmail.com>
2023-12-05 14:18:21 -0500

    dir nesting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LessonsList from '../components/LessonsList';
+import LessonsList from './components/LessonsList';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/lessons" />
+        <Route path="/lessons" element={<LessonsList />}/>
         <Route path="/calendar" />
         {/* Add admin routes */}
       </Routes>

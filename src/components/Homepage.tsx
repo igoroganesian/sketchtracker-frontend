5a61e2fb2d5277c040da7c14109425586210b0ca
Igor Oganesian <igor.oganesian@gmail.com>
2023-12-11 21:34:45 -0500

    css experiment

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,14 +1,16 @@
 import './Homepage.css';
 
 const HomePage = () => {
-    return (
-      <div className="homePage-container">
-        <h1>Sketch Tracker</h1>
+  return (
+    <div className="homePage-container">
+      <h1>Sketch Tracker</h1>
+      <div className='homePage-container-buttons'>
         <a href='/lessons' className='link-button'>Lessons</a>
         <a href='/calendar' className='link-button'>Calendar</a>
         <a href='https://drawabox.com/' className='link-button'>Drawabox</a>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default HomePage;
+export default HomePage;
